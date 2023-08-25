203dfdeed01cb9ea0daf261af887786a6aaec25b
dwielok <dwieloknuraini773@gmail.com>
2023-08-25 09:47:40 +0700

    contoh pdf

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/test_pdf', [App\Http\Controllers\PdfController::class, 'test_pdf']);
