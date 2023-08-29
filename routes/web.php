cda5f3305b01a2a9188f7531bb354711124623bd
Krismawati <krismawatti11@gmail.com>
2023-08-29 10:27:19 +0700

    Route & Controller

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,11 @@
 <?php
 
+use App\Http\Controllers\KaryawanPerbantuanInkaController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\KaryawanPKWTController;
+use App\Http\Controllers\KaryawanTetapController;
 use App\Http\Controllers\listTetap;
 
 /*
@@ -30,5 +33,19 @@ Route::post('actionlogin', [LoginController::class, 'actionlogin'])->name('actio
 Route::get('dashboard', [DashboardController::class, 'index'])->name('dashboard');
 Route::get('actionlogout', [LoginController::class, 'actionlogout'])->name('actionlogout')->middleware('auth');
 
+//route halaman karyawan tetap
+Route::get('KaryawanTetap', [KaryawanTetapController::class,'index'])->name('KaryawanTetap');
+Route::get('ViewTetap', [viewTetapController::class, 'index'])->name('KaryawanTetap');
+Route::get('View',[actionViewTetapController::class, 'index'])->name('actionViewTetap');
 
-Route::get('listtetap', [listTetapController::class,'index'])->name('listTetap');
+
+
+//route halaman karyawan perbantuan Inka
+Route::get('karyawanperbantuaninka', [KaryawanPerbantuanInkaController::class, 'index'])->name('karyawanperbantuaninka');
+
+
+//route halaman karyawan PKWT
+
+Route::get('karyawanPKWT', [KaryawanPKWTController::class, 'index'])->name('karyawanPKWT');
+
+
