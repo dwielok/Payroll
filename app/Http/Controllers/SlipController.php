0c623c001f73fd2fd743351185ca3f75a4e4ed11
dwielok <dwieloknuraini773@gmail.com>
2023-08-29 20:00:48 +0700

    tampilan karyawan,rekap,slip

--- app/Http/Controllers/SlipController.php
+++ app/Http/Controllers/SlipController.php
@@ -4,19 +4,9 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-<<<<<<< HEAD
-class SlipController extends Controller
-{
-    public function index()
-    {
-        return view('slip_gaji');
-    }
-}
-=======
 class SlipController extends Controller{
     public function index()
     {
         return view('slip');
     }
 }
->>>>>>> ad06b6e193c1543e7283235e0450eda5fea269ce
