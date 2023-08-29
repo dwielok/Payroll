4ca276cc699112506252d2dd57189dccabe9b91c
aldanelva <aldamondel@gmail.com>
2023-08-29 12:56:24 +0700

    -

--- routes/web.php
+++ routes/web.php
@@ -5,15 +5,9 @@ use App\Http\Controllers\KaryawanTetapController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\DashboardController;
-<<<<<<< HEAD
 use App\Http\Controllers\TemplateController;
 use App\Http\Controllers\listTetap;
-=======
 use App\Http\Controllers\KaryawanPKWTController;
-use App\Http\Controllers\listTetap;
-
-
->>>>>>> 6ac97e064d82c7b9b1040ababc8e1f14308c229e
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -38,22 +32,18 @@ Route::post('actionlogin', [LoginController::class, 'actionlogin'])->name('actio
 // Route::get('dashboard', [DashboardController::class, 'index'])->name('dashboard')->middleware('auth');
 Route::get('dashboard', [DashboardController::class, 'index'])->name('dashboard');
 Route::get('actionlogout', [LoginController::class, 'actionlogout'])->name('actionlogout')->middleware('auth');
-<<<<<<< HEAD
 Route::get('template', [TemplateController::class, 'index'])->name('template');
 Route::get('listtetap', [listTetapController::class, 'index'])->name('listTetap');
-=======
 
 
-Route::get('listtetap', [listTetapController::class,'index'])->name('listTetap');
+Route::get('listtetap', [listTetapController::class, 'index'])->name('listTetap');
 
 //Routing Halaman Karyawan Tetap
-Route::get('KaryawanTetap',[KaryawanTetapController::class, 'index'])->name('karyawantetap');
+Route::get('KaryawanTetap', [KaryawanTetapController::class, 'index'])->name('karyawantetap');
 
 
 //Routing Halaman Perbantuan Inka
-Route::get('karyawanperbantuaninka',[KaryawanPerbantuanInkaController::class, 'index'])->name('karyawanperbantuaninka');
+Route::get('karyawanperbantuaninka', [KaryawanPerbantuanInkaController::class, 'index'])->name('karyawanperbantuaninka');
 
 //Routing Halaman PKWT
-Route::get('karyawanPKWT',[KaryawanPKWTController::class, 'index'])->name('karyawanPKWT');
-
->>>>>>> 6ac97e064d82c7b9b1040ababc8e1f14308c229e
+Route::get('karyawanPKWT', [KaryawanPKWTController::class, 'index'])->name('karyawanPKWT');
