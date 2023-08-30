b300a6b62be9b4f213ccfd0f80a47f3a96055d03
Krismawati <krismawatti11@gmail.com>
2023-08-30 08:52:26 +0700

    rekap gaji

--- app/Http/Controllers/SlipController.php
+++ app/Http/Controllers/SlipController.php
@@ -3,13 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
-<<<<<<< HEAD
-class SlipController extends Controller
-{
-=======
 class SlipController extends Controller{
->>>>>>> f61de03e6cfa5557560a4e8b65746b0a5c0687eb
     public function index()
     {
         return view('slip');
