451fd77f2022d793b517703180136bb24999d183
aldanelva <aldamondel@gmail.com>
2023-08-31 08:28:36 +0700

    import

--- resources/views/import_tetap.blade.php
+++ resources/views/import_tetap.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.connect')
 
 @section('content')
-<div class="page-wrapper">
+    <div class="page-wrapper">
         <div class="page-titles">
             <div class="row">
                 <div class="col-lg-8 col-md-6 col-12 align-self-center">
@@ -10,42 +10,49 @@
                             <li class="breadcrumb-item">
                                 <a href="index.html" class="link"><i data-feather="grid"></i></a>
                             </li>
-                            <li class="breadcrumb-item active" aria-current="page">
+                            {{-- <li class="breadcrumb-item active" aria-current="page">
                                 Import Data
-                            </li>
+                            </li> --}}
                         </ol>
                     </nav>
                     <h1 class="mb-0 fw-bold">Import Data</h1>
                 </div>
 
                 <div class="container-fluid">
-            <!-- ============================================================= -->
-            <!-- Start Page Content -->
-            <!-- ============================================================= -->
-            <div class="row">
-                <div class="col-12">
-                    <div class="card">
-                        <div class="card-body">
-                            <div
-                                class="col-lg-12 col-md-12 d-none d-md-flex align-items-center justify-content-between mb-0">
-                   
+                    <!-- ============================================================= -->
+                    <!-- Start Page Content -->
+                    <!-- ============================================================= -->
+                    <div class="row">
+                        <div class="col-12">
+                            <div class="card">
+                                <div class="card-body">
+                                    <div
+                                        class="col-lg-12 col-md-12 d-none d-md-flex align-items-center justify-content-between mb-0">
 
-<<<<<<< HEAD
-    <div class="container">
-        {{-- <center>
-            <h4>Import Data</h4>
-            <h5><a target="_blank" href="https://www.malasngoding.com/">www.malasngoding.com</a></h5>
-        </center> --}}
 
-        <h1 class="mb-0 fw-bold">Import Data</h1>
-=======
-                                <div class="container">
-                                    <div class="row mt-0">
->>>>>>> 79d81db3dc56f7809bb359aa41912cf2b2772b6d
+                                        <div class="container">
 
-                                        <div class="col-md-8">
-                                            <h2 class="text-left mb-3 mt-3" >Upload Or Import Data</h2>
-                                            <button type="button" class="btn btn-navy mr-5" data-toggle="modal" data-target="#importExcel">Upload File</button>
+                                            <div class="container">
+                                                <div class="row mt-0">
+
+                                                    <div class="col-md-8">
+                                                        <h2 class="text-center mb-3 mt-3 " style="">Upload Or Import
+                                                            Data</h2>
+
+
+                                                    </div>
+
+                                                    <div class="d-flex center-content-end">
+                                                        <button type="button"
+                                                            class="btn btn-navy d-flex align-items-center ms-2"
+                                                            style="" data-toggle="modal"
+                                                            data-target="#importExcel">Upload
+                                                            File</button>
+
+                                                    </div>
+                                                </div>
+                                            </div>
+
                                         </div>
                                     </div>
                                 </div>
@@ -54,9 +61,5 @@
                     </div>
                 </div>
             </div>
-                </div>
-            </div>
         </div>
-    </div>
-@endsection
-
+    @endsection
