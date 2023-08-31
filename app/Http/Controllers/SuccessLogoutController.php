c163ad12ba712fe67385ce1e261331d0c863031b
aldanelva <aldamondel@gmail.com>
2023-08-31 14:38:58 +0700

    successlogout

--- app/Http/Controllers/SuccessLogoutController.php
+++ app/Http/Controllers/SuccessLogoutController.php
@@ -4,10 +4,10 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class ImportFileController extends Controller
+class SuccessLogoutController extends Controller
 {
     public function index()
     {
-        return view('import_inka');
+        return view('successlogout');
     }
 }
