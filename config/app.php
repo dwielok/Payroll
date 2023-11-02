3237c682c2a4f00f038bc081b8f1f789cdcbefa5
aldanelva <aldamondel@gmail.com>
2023-11-02 14:40:03 +0700

    ---

--- config/app.php
+++ config/app.php
@@ -162,6 +162,7 @@ return [
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
         Maatwebsite\Excel\ExcelServiceProvider::class,
+        // Barryvdh\DomPDF\ServiceProvider::class,
 
 
         /*
@@ -242,6 +243,7 @@ return [
         'View' => Illuminate\Support\Facades\View::class,
         //'Excel' => \Maatwebsite\Excel\Facades\Excel::class,
         'Excel' => Maatwebsite\Excel\Facades\Excel::class,
+        // 'PDF' => Barryvdh\DomPDF\Facade::class,
 
     ],
 
